7a843f693d4925ebba8cb804186b60104368d015
Peter Jung <peter.jung@heureka.cz>
2020-05-29 07:58:24 +0200

    More documentation

--- Sources/XGBoost/Enums.swift
+++ Sources/XGBoost/Enums.swift
@@ -1,43 +1,51 @@
 import Foundation
 
-public
-typealias Parameter = (name: String, value: String)
+/// Parameter for XGBoost.
+/// See https://xgboost.readthedocs.io/en/latest/parameter.html for available parameters.
+public typealias Parameter = (name: String, value: String)
 
-public
-typealias Version = (major: Int, minor: Int, patch: Int)
+/// Typealias for underlying XGBoost version.
+public typealias Version = (major: Int, minor: Int, patch: Int)
 
-public
-typealias Feature = (name: String, type: FeatureType)
+/// Information about feature.
+public typealias Feature = (name: String, type: FeatureType)
 
-public
-typealias BufferModel = (length: UInt64, data: UnsafeMutablePointer<UnsafePointer<Int8>?>)
+/// Tuple holding length of buffer along with it, so it can be easily read.
+public typealias BufferModel = (length: UInt64, data: UnsafeMutablePointer<UnsafePointer<Int8>?>)
 
-public
-typealias Shape = (row: Int, column: Int)
+/// Shape of Data.
+public typealias Shape = (row: Int, column: Int)
 
 public enum AfterIterationCallbackOutput {
     case stop, next
 }
 
+/// Type of feature.
 public enum FeatureType: String {
     case quantitative = "q"
     case indicator = "i"
 }
 
+/// Currently supported model formats.
 public enum ModelFormat: String {
     case text, json
 }
 
+/// Currently supported data formats.
 public enum DataFormat: String {
     case binary, csv
 }
 
+/// Predefined names of float fields settable in Data.
+/// You can set also another fields using method accepting strings.
 public enum FloatField: String {
     case label
     case weight
     case baseMargin = "base_margin"
 }
 
+/// Predefined names of uint fields settable in Data.
+/// You can set also another fields using method accepting strings.
 public enum UIntField: String {
     case group
 }
@@ -50,6 +58,7 @@ public enum Importance: String {
     case totalCover = "total_cover"
 }
 
+/// Booster types supported by XGBoost
 public enum Booster: String {
     case gbtree
     case gblinear
