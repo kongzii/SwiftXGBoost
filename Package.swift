4bd828ddb84cf538b3fd67c73412588d7e3f6521
Peter Jung <peter.jung@heureka.cz>
2020-05-30 09:03:32 +0200

    Lower required OSX version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftXGBoost",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v10_13),
     ],
     products: [
         .library(
