ab376e226b8235ae00e037f094640e37964d8e3e
Peter Jung <peter.jung@heureka.cz>
2020-06-06 11:39:54 +0200

    Tensorflow extensions

--- Sources/XGBoost/TensorFlow.swift
+++ Sources/XGBoost/TensorFlow.swift
@@ -2,11 +2,19 @@
 
     import TensorFlow
 
-    extension Tensor: FloatData, ShapeData where Scalar == Float {
+    extension Tensor: FloatData where Scalar == Float {
         public func data() throws -> [Float] {
             scalars
         }
+    }
 
+    extension Tensor: UInt32Data where Scalar == UInt32 {
+        public func data() throws -> [UInt32] {
+            scalars
+        }
+    }
+
+    extension Tensor: ShapeData {
         public func dataShape() throws -> Shape {
             if shape.count == 1 {
                 return try Shape(row: 1, column: shape[0])
