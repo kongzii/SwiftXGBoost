0ae494d709f52d627fc14e05d1c9870f82925937
Peter Jung <peter.jung@heureka.cz>
2020-06-15 20:09:20 +0200

    Parameter docs

--- Sources/XGBoost/Parameter.swift
+++ Sources/XGBoost/Parameter.swift
@@ -1,4 +1,4 @@
-/// Parameter for Booster.
+/// Struct representating parameter for Booster.
 /// See https://xgboost.readthedocs.io/en/latest/parameter.html for available parameters.
 public struct Parameter: Equatable, Codable {
     var count: Int = 0
@@ -6,11 +6,15 @@ public struct Parameter: Equatable, Codable {
     public let name: String
     public let value: String
 
+    /// - Parameter: Name of parameter.
+    /// - Parameter: Value for parameter, can be anything convertible to string.
     public init(_ name: String, _ value: CustomStringConvertible) {
         self.name = name
         self.value = "\(value)"
     }
 
+    /// - Parameter name: Name of parameter.
+    /// - Parameter value: Value for parameter, can be anything convertible to string.
     public init(name: String, value: CustomStringConvertible) {
         self.name = name
         self.value = "\(value)"
