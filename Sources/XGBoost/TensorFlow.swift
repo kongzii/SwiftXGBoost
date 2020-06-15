52819019a34df511898fa1f133d11e0e8d0c67dc
Peter Jung <peter.jung@heureka.cz>
2020-06-15 20:15:38 +0200

    Tensorflow docs

--- Sources/XGBoost/TensorFlow.swift
+++ Sources/XGBoost/TensorFlow.swift
@@ -3,18 +3,21 @@
     import TensorFlow
 
     extension Tensor: FloatData where Scalar == Float {
+        /// Comfortance for FloatData.
         public func data() throws -> [Float] {
             scalars
         }
     }
 
     extension Tensor: UInt32Data where Scalar == UInt32 {
+        /// Comfortance for UInt32Data.
         public func data() throws -> [UInt32] {
             scalars
         }
     }
 
     extension Tensor: ShapeData {
+        /// Comfortance for ShapeData.
         public func dataShape() throws -> Shape {
             if shape.count == 1 {
                 return try Shape(row: 1, column: shape[0])
