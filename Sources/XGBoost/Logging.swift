4457cb3989e7583bbfeb921722de0aacfe5753d9
Peter Jung <peter.jung@heureka.cz>
2020-06-15 20:08:26 +0200

    Logging docs

--- Sources/XGBoost/Logging.swift
+++ Sources/XGBoost/Logging.swift
@@ -1,7 +1,9 @@
 import Logging
 
+/// Logger instance used for messages.
 let logger = Logger(label: "swiftxgboost")
 
+/// - Parameters: Message to log.
 func log(_ message: String) {
     // TODO: Rabit logging.
     logger.info("\(message)")
