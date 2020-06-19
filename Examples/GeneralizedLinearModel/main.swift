7710b6959b27b22f59c690c0a077d94a9ef83875
Peter Jung <peter.jung@heureka.cz>
2020-06-19 14:20:18 +0200

    Add comments in GLM

--- Examples/GeneralizedLinearModel/main.swift
+++ Examples/GeneralizedLinearModel/main.swift
@@ -3,6 +3,9 @@
 import Foundation
 import XGBoost
 
+// This script demonstrate how to fit generalized linear model in xgboost
+// basically, we are using linear model, instead of tree for our boosters
+
 let train = try DMatrix(
     name: "train",
     from: "Examples/Data/agaricus.txt.train",
@@ -14,13 +17,24 @@ let test = try DMatrix(
     format: .libsvm
 )
 
-let parameters = [
+
+// change booster to gblinear, so that we are fitting a linear model
+// alpha is the L1 regularizer
+// lambda is the L2 regularizer
+// you can also set lambda_bias which is L2 regularizer on the bias term
+var parameters = [
     Parameter("seed", 0),
     Parameter("lambda", 1),
     Parameter("alpha", 0.0001),
     Parameter("booster", "gblinear"),
     Parameter("objective", "binary:logistic"),
 ]
+
+// normally, you do not need to set eta (step_size)
+// XGBoost uses a parallel coordinate descent algorithm (shotgun),
+// there could be affection on convergence with parallelization on certain cases
+// setting eta to be smaller value, e.g 0.5 can make the optimization more stable
+// parameters.append(Parameter("eta", 1))
 
 let booster = try Booster(
     with: [train, test],
